55fdaa4b2b7013a94510a1ad02564e843f1fd0aa
Daniel <49683346+0tii@users.noreply.github.com>
2022-04-19 10:42:46 +0200

    GET '/' working

--- start.js
+++ start.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import { generateGuid } from './lib/gen.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
-
+    return res.send(generateGuid());
 });
+
+app.listen(3000, () => console.log('listening on port 3000...'));
