95718170707f2469f89fc16be8d2267fb3798773
Daniel <49683346+0tii@users.noreply.github.com>
2022-05-13 15:50:04 +0200

    ...

--- Database/setup.sql
+++ Database/setup.sql
@@ -9,11 +9,11 @@ USE getguid;
 DROP TABLE IF EXISTS guids;
 
 CREATE TABLE IF NOT EXISTS guids(
-    unique_id VARCHAR(32) NOT NULL UNIQUE PRIMARY KEY
+    unique_id VARCHAR(35) NOT NULL UNIQUE PRIMARY KEY
 );
 
 CREATE INDEX idx_uid ON guids(unique_id);
 
-INSERT INTO guids (unique_id) VALUES ('00000000000000000000000000000000');
-INSERT INTO guids (unique_id) VALUES ('11111111111111111111111111111111');
-INSERT INTO guids (unique_id) VALUES ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+INSERT INTO guids (unique_id) VALUES ('00000000-0000-0000-0000-00000000000');
+INSERT INTO guids (unique_id) VALUES ('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa');
+INSERT INTO guids (unique_id) VALUES ('bf6697d6-44fe-4501-5cc1-17f2563e88c');
--- cfg/_config.js
+++ cfg/_config.js
@@ -4,10 +4,14 @@ GetGuid Rest API
 License: MIT
 © Daniel H. Rauhut 2022
 */
+
+/*
+This is the default config file shipped. rename this to config.js for it to take effect.
+*/
 export default {
     dbHost: 'localhost',
     dbUser: 'root',
-    dbPort: 3306, //for unix
+    dbPort: '/var/run/mysqld/mysqld.sock', //for windows use 3306
 
     maxConsecutiveCollisions: 10,  //consecutive collisions should never be a thing, if this threshold (> 1) is exceeded, there is an error in the code.
     getPath: "/guid",
